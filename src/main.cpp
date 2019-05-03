21a745301d670a7426e9e9a527824d8efbc6478d
20zinnm <meyerzinn@gmail.com>
2019-05-03 13:10:19 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <DNSServer.h>
+#include <WebServer.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Update.h>
+#define VARIANT "esp32"
+#include <WiFiManager.h>
+#define USE_SERIAL Serial
+
+#define CURRENT_VERSION VERSION
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
